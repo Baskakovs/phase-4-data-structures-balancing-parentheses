00a39656f396417974ca4d41a64f7c777ec7c710
Baskakovs <baskakovs@me.com>
2023-04-11 12:49:37 +0100

    commit tree 867624672f6c540a6d01b018db4cf022788c1bc0

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,35 @@
 require_relative './stack'
+require 'byebug'
+
+def balancing_parentheses(string)
+    stack = Stack.new
+    string.chars.each do |c|
+        stack.push(c)
+    end
+    # byebug
+    missing_parenthesis = 0
+    if stack.search("(") == -1 || stack.search(")") == -1
+        stack.size
+    elsif stack.peek == "(" && stack.peek_first == ")"
+        missing_parenthesis += 2
+    elsif stack.peek == ")" && stack.peek_first != "("
+        missing_parenthesis += 1
+    elsif stack.peek_first == "(" && stack.peek != ")"
+        missing_parenthesis += 1
+    elsif stack.peek == ")" && stack.peek_first == "("
+        # byebug
+        stack.pop
+        stack.shift
+        stack.show.each do |c|
+            if c == ")"
+                missing_parenthesis += 1 unless stack.search("(") != -1
+            elsif c == "("
+                missing_parenthesis += 1 unless stack.search(")") != -1
+            end
+        end 
+        missing_parenthesis
+    end
+end
+
 
 # your code here
--- lib/stack.rb
+++ lib/stack.rb
@@ -12,12 +12,31 @@ class Stack
     @stack.pop
   end
 
+  def shift
+    @stack.shift
+  end
+
   def peek
     @stack.last
+  end
+
+  def peek_first
+    @stack.first
   end
 
   def size
     @stack.length
   end
 
+  def show
+    @stack
+  end
+
+  def search(value)
+    if @stack.index(value).nil?
+      -1
+    else
+      @stack.length - 1 - @stack.index(value)
+    end
+  end
 end
